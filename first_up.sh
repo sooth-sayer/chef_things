9bbc5f0917de69787e9e455efe236e04fbd463ea
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2013-11-09 17:07:54 +0400

    Remove old ruby

--- first_up.sh
+++ first_up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install ruby1.9.3 build-essential wget libruby1.9.3 rubygems
+apt-get -y remove ruby
+apt-get -y install ruby1.9.3 build-essential wget
 gem update --no-rdoc --no-ri
 gem install ohai chef --no-rdoc --no-ri
