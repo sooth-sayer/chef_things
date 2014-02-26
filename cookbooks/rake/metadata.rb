29ac56ccbf132e57801bda9abfe02e00735840d1
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2014-02-26 17:00:00 +0400

    rake cookbook from cookbooks repo

--- cookbooks/rake/metadata.rb
+++ cookbooks/rake/metadata.rb
@@ -1,12 +1,7 @@
 name             "rake"
-maintainer       "NoNamed"
-maintainer_email "Empty"
-license          "Apache 2.0"
-description      "Installs rake"
+maintainer       "Ernie Brodeur"
+maintainer_email "ebrodeur@ujami.net"
+license          "Beer-ware rv42"
+description      "Installs/Configures rake"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-
-%w{debian ubuntu arch}.each do |os|
-  supports os
-end
-
+version          "0.1.1"
